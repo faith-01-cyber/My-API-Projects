53b723f103e936739cb9dbf4f70c524842dbce26
Faith Kemboi <kemboifaith941@gmail.com>
2026-02-28 13:21:31 +0300

    Clean repo: add only project files

--- control-flow/multiplication_table.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    number = int(input("Enter a number to see its multiplication table: "))
-    for i in range(1, 11):
-        print(f"{number} * {i} = {number * i}")
-
-if __name__ == "__main__":
-    main()
